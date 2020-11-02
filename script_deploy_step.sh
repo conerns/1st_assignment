0905e13503ac0e1b878445a02268f29112b849ee
aliromagnoli <aliceromagnoli98@gmail.com>
2020-11-02 14:28:34 +0000

    deploy con host

--- script_deploy_step.sh
+++ script_deploy_step.sh
@@ -1,5 +1,5 @@
 set -xe
-ssh -o PreferredAuthentications=publickey azureuser@52.164.218.129 \
+ssh host PreferredAuthentications=publickey azureuser@52.164.218.129 \
 "docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY \
 && docker rm --force pipeline || true \
 && docker pull $CONTAINER_IMAGE:$CI_COMMIT_REF_NAME \
