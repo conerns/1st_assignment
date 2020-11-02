237ee228ec5e6ffe48e7f903013efc318c7371d7
Andrei G. Taraboi <andrei.taraboi@gmail.com>
2020-11-02 22:43:44 +0000

    Update script_deploy_step.sh

--- script_deploy_step.sh
+++ script_deploy_step.sh
@@ -1,7 +1,7 @@
 set -xe
 ssh -o PreferredAuthentications=publickey azureuser@13.73.147.152 \
 "docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY \
-&& docker rm --force pipeline || true \
+&& docker rm --force pipeline-1.0-SNAPSHOT || true \
 && docker pull $CONTAINER_IMAGE:$CI_COMMIT_REF_NAME \
-&& docker run -d --restart on-failure:5 --name pipeline $CONTAINER_IMAGE:$CI_COMMIT_REF_NAME \
+&& docker run -d --restart on-failure:5 --name pipeline-1.0-SNAPSHOT $CONTAINER_IMAGE:$CI_COMMIT_REF_NAME \
 && exit"
